7e7f22330e9e3758160d0223cccdd2123d2e5685
SofyaIlina <kenny8556@yandex.ru>
2023-02-15 03:17:29 +0500

    добавляет стили до каталога

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,15 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.site-list');
+const toggle = document.querySelector('.header__toggle');
+
+// Бургер-меню
+
+  nav.classList.remove('site-list--no-js');
+  toggle.classList.remove('header__toggle--no-js');
+
+  const toggleMenu = () => {
+    nav.classList.toggle('site-list--close');
+    toggle.classList.toggle('header__toggle--close');
+    toggle.classList.toggle('header__toggle--open');
+  }
+
+  toggle.addEventListener('click', toggleMenu);
